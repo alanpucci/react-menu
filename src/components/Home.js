807c24068a8206c330ebcb4f5666bcfdc1ffaaca
Alejandro Sobko <alejandro.sobko@gmail.com>
2020-08-30 17:45:22 -0300

    Agrego logica para mostrar pedidos

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Boton = styled.button `
 
 const Home = ({productosTotal, pizza, ensalada, pasta, milanesa}) => {
 
-    
+
     const [ show, setShow ] = useState(false);
     const [ total , setTotal ] = useState(0);
 
@@ -29,7 +29,15 @@ const Home = ({productosTotal, pizza, ensalada, pasta, milanesa}) => {
         }
     }
 
-    return ( 
+    const mostrarPedidos = () => {
+        const cantidad = productosTotal.reduce((result, e) => {
+            result[e.name] = (result[e.name] || 0) + 1
+            return result
+        }, {})
+        console.log(cantidad)
+    }
+
+    return (
         <div>
             <h2>Elige algun menu</h2>
             <h3>El total de productos es: {productosTotal.length}</h3>
@@ -37,11 +45,11 @@ const Home = ({productosTotal, pizza, ensalada, pasta, milanesa}) => {
             <h4>El total de ensaladas es: {ensalada.length} </h4>
             <h4>El total de pastas es: {pasta.length}</h4>
             <h4>El total de milanesas es: {milanesa.length}</h4>
-            <button type="button" className="btn btn-primary btn-lg my-4" onClick={() => mostrarEnsaladas()}>Mostrar pedidos</button> <br />
+            <button type="button" className="btn btn-primary btn-lg my-4" onClick={() => mostrarPedidos()}>Mostrar pedidos</button> <br />
             <Boton type="button" className="btn btn-primary btn-sm mb-4" onClick={() => precioTotal()}>Mostrar total</Boton>
             {show ? <h2 className="d-inline ml-3 bg-dark rounded text-info ">${total}</h2> : null}
         </div>
      );
 }
- 
+
 export default Home;
